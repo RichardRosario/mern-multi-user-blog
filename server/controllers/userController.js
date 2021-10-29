b29616e52bcc85123a867f416f62df71573bbd44
Richard Rosario <catrosits@gmail.com>
2021-10-29 19:56:59 +0800

    creating the login logic and generating token using jsonwebtoken

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,9 @@
 import User from "../models/userModel.js";
 import { validationResult } from "express-validator";
 import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
 
+// ===========================
 // POST, /api/users/register
 // public route
 export const register = async (req, res) => {
@@ -27,6 +29,44 @@ export const register = async (req, res) => {
 		res
 			.status(200)
 			.json({ msg: "Register process successful! Please signin..." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).json({ msg: "Server Error..." });
+	}
+};
+
+// ========================
+// POST, /api/users/login
+// public route
+export const login = async (req, res) => {
+	try {
+		const { email, password } = req.body;
+		// check of user exist
+		const user = await User.findOne({ email });
+		if (!user) {
+			res.status(401).res.json({
+				msg: "There is no user of that email, please register an account."
+			});
+		}
+		// authenticate the user
+		if (!user.authenticate(password)) {
+			res.status(400).json({ msg: "Wrong email and password" });
+		}
+
+		// generate a token and send to client
+		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+			expiresIn: "1d"
+		});
+
+		res.cookie("token", token, { expiresIn: "1d" });
+		res.json({
+			_id: user._id,
+			username: user.username,
+			name: user.name,
+			email: user.email,
+			role: user.role,
+			token
+		});
 	} catch (error) {
 		console.log(error.message);
 		res.status(500).json({ msg: "Server Error..." });
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import registerValidation from "../validators/userValidator.js";
 
-import { register } from "../controllers/userController.js";
+import { register, login } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerValidation, register);
+router.post("/login", login);
 
 export default router;
