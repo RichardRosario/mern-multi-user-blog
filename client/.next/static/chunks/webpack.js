05d5adcc7a56f77c5a075f997624625685e36d46
Richard Rosario <catrosits@gmail.com>
2021-11-01 19:04:09 +0800

    creating the frontend signin functionality

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13d01822c35d0f54"; }
+/******/ 		__webpack_require__.h = function() { return "ff79372d22399db2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,12 +1,15 @@
 import Layout from "../components/Layout";
 import SignupComponent from "../components/auth/SignupComponent";
-import Link from "next/link";
 
 const signup = () => {
 	return (
 		<Layout>
 			<h2>Sign Up for an Account!</h2>
-			<SignupComponent />
+			<div className='row'>
+				<div className='col-md-6 offset-md-3'>
+					<SignupComponent />
+				</div>
+			</div>
 		</Layout>
 	);
 };
